ca136bf0632c4ec05964ff0b661a3f94a963457e
Will Schroeder <will.schroeder@dollarshaveclub.com>
2019-12-01 16:05:59 -0800

    some python working

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import execa = require('execa');
+import {PythonRepo} from "./repo/python"
 
 (async () => {
     try {
-        const {stdout} = await execa('python3', ['./py_src/sentence_tokenizer.py', "1 cup parsley", "1 tsp basil"])
-        console.log(stdout);
+        const ingredients = ["1 cup parsley", "1 tsp basil"]
+        const repo = new PythonRepo()
+        const sentences = await repo.tokenize(ingredients)
+        console.log(sentences[0][0])
     }
     catch (e) {
         console.log(e)
