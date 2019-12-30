72456e76c77e34dc0db5085a60dd7b7c044af5b8
Will Schroeder <MY_NAME@example.com>
2019-12-30 15:55:41 -0800

    recipe scraper

--- py_src/recipe_scraper.py
+++ py_src/recipe_scraper.py
@@ -7,6 +7,7 @@ import json
 import sys
 import requests
 from bs4 import BeautifulSoup
+import os
 
 def all_recipes(url):
     page = requests.get(url)
@@ -25,19 +26,20 @@ def all_recipes(url):
 
     return {"ingredients": ingredients, "steps": steps}
 
-print("{\"ingredients\":[\"2 cups all-purpose flour\",\"2 teaspoons baking powder\",\"1 teaspoon salt\",\"1/4 teaspoon baking soda\",\"7 tablespoons unsalted butter, chilled in freezer and cut into thin slices\",\"3/4 cup cold buttermilk\",\"2 tablespoons buttermilk for brushing\"],\"steps\":[\"Preheat oven to 425 degrees F (220 degrees C).\",\"Line a baking sheet with a silicone baking mat or parchment paper.\",\"Whisk flour, baking powder, salt, and baking soda together in a large bowl.\",\"Cut butter into flour mixture with a pastry blender until the mixture resembles coarse crumbs, about 5 minutes.\",\"Make a well in the center of butter and flour mixture. Pour in 3/4 cup buttermilk; stir until just combined.\",\"Turn dough onto a floured work surface, pat together into a rectangle.\",\"Fold the rectangle in thirds. Turn dough a half turn, gather any crumbs, and flatten back into a rectangle. Repeat twice more, folding and pressing dough a total of three times.\",\"Roll dough on a floured surface to about 1/2 inch thick.\",\"Cut out 12 biscuits using a 2 1/2-inch round biscuit cutter.\",\"Transfer biscuits to the prepared baking sheet. Press an indent into the top of each biscuit with your thumb.\",\"Brush the tops of biscuits with 2 tablespoons buttermilk.\",\"Bake in the preheated oven until browned, about 15 minutes.\"]}")
+if os.getenv('IS_HEROKU', "false") == "true":
+    # print(all_recipes('https://www.allrecipes.com/recipe/220943/chef-johns-buttermilk-biscuits/'))
+    args = sys.argv[1:]
+    site = args[0]
+    site_url = args[1]
 
-# print(all_recipes('https://www.allrecipes.com/recipe/220943/chef-johns-buttermilk-biscuits/'))
+    if not site or not site_url:
+        print(json.dumps({"error": "Need a website and a url as params"}))
+        exit(1)
 
-# args = sys.argv[1:]
-# site = args[0]
-# site_url = args[1]
-#
-# if not site or not site_url:
-#     print(json.dumps({"error": "Need a website and a url as params"}))
-#     exit(1)
-#
-# if site == "allrecipes":
-#     print(json.dumps(all_recipes(site_url)))
-#
-# exit(0)
+    if site == "allrecipes":
+        print(json.dumps(all_recipes(site_url)))
+
+    exit(0)
+else:
+    print("{\"ingredients\":[\"2 cups all-purpose flour\",\"2 teaspoons baking powder\",\"1 teaspoon salt\",\"1/4 teaspoon baking soda\",\"7 tablespoons unsalted butter, chilled in freezer and cut into thin slices\",\"3/4 cup cold buttermilk\",\"2 tablespoons buttermilk for brushing\"],\"steps\":[\"Preheat oven to 425 degrees F (220 degrees C).\",\"Line a baking sheet with a silicone baking mat or parchment paper.\",\"Whisk flour, baking powder, salt, and baking soda together in a large bowl.\",\"Cut butter into flour mixture with a pastry blender until the mixture resembles coarse crumbs, about 5 minutes.\",\"Make a well in the center of butter and flour mixture. Pour in 3/4 cup buttermilk; stir until just combined.\",\"Turn dough onto a floured work surface, pat together into a rectangle.\",\"Fold the rectangle in thirds. Turn dough a half turn, gather any crumbs, and flatten back into a rectangle. Repeat twice more, folding and pressing dough a total of three times.\",\"Roll dough on a floured surface to about 1/2 inch thick.\",\"Cut out 12 biscuits using a 2 1/2-inch round biscuit cutter.\",\"Transfer biscuits to the prepared baking sheet. Press an indent into the top of each biscuit with your thumb.\",\"Brush the tops of biscuits with 2 tablespoons buttermilk.\",\"Bake in the preheated oven until browned, about 15 minutes.\"]}")
+
